ffd6ce10497b2afa09549cd72eca4f045ed36cad
pmbrian <pmbrian@hotmail.com>
2024-08-15 01:04:57 -0600

    estetica codigo

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# ComerciaizadoraDeAlimentos
- Programa para venta de alimentos a mayoreo Programado en python
+# Comercializadora de alimentos
+
+código de administración de un punto de venta básico
+incluye las funciones de:
+- Compras
+- Pedidos
+- Registro de inventarios
+- Registro de actividades
+- Generación de archivos pdf, csv y excel
+- Registro de clientes
+
+### Librerías necesarias
+
+El programa se desarrollo en python 3.9 y requiere de librerías que no están incluidas en la versión de python 
+
+- ReportLab `pip install reportlab`
+
+- Pandas `pip install pandas`
+
+- Matplotlib `pip install matplotlib`
+
+- tkcalendar `pip install tkcalendar`
--- Vistas/MenuPrincipal.py
+++ Vistas/MenuPrincipal.py
@@ -2,35 +2,36 @@ import tkinter as tk
 from tkinter import messagebox
 
 
-def menu_ventas(nivel, ventana,id):
+def menu_ventas(nivel, ventana, id):
     import Clases.Productos as prod
     if prod.is_empty():
-        messagebox.showerror("Cuidado...","No se pueden realizar ventas sin ningun producto registrado")
+        messagebox.showerror(
+            "Cuidado...", "No se pueden realizar ventas sin ningun producto registrado")
     else:
         ventana.destroy()
         import Vistas.VistaVentas as vv
         vv.main(nivel, id)
 
 
-def menu_inventarios(nivel, ventana,id):
+def menu_inventarios(nivel, ventana, id):
     ventana.destroy()
     import Vistas.VistaProductos as vp
-    vp.main(nivel,id)
+    vp.main(nivel, id)
 
 
-def generacion_reportes(nivel,ventana, id):
+def generacion_reportes(nivel, ventana, id):
     ventana.destroy()
     import Vistas.VistaReportes as vrp
-    vrp.main(nivel,id)
+    vrp.main(nivel, id)
 
 
-def gestion_usuarios(nivel,ventana, id):
+def gestion_usuarios(nivel, ventana, id):
     ventana.destroy()
     import Vistas.VistaUsuarios as vu
     vu.main(nivel, id)
 
 
-def gestion_clientes(nivel,ventana, id):
+def gestion_clientes(nivel, ventana, id):
     ventana.destroy()
     import Vistas.VistaClientes as vc
     vc.main(nivel, id)
@@ -54,39 +55,59 @@ def salir(ventana):
     ventana.destroy()
 
 
-def main(nivel,id):
+def main(nivel, id):
     ventana = tk.Tk()
     ventana.title("Menú de Opciones")
-    ventana.geometry("300x400")
+    ventana.geometry("580x200")
 
-    btn_menu_ventas = tk.Button(ventana, text="Menú de Ventas", command=lambda: menu_ventas(nivel, ventana,id))
-    btn_menu_ventas.pack(pady=10)
+    # Definir el tamaño uniforme para los botones
+    btn_width = 20  # Ancho del botón
+    btn_height = 2  # Alto del botón
 
-    btn_menu_inventarios = tk.Button(ventana, text="Menú de Inventarios",
-                                     command=lambda: menu_inventarios(nivel, ventana,id))
-    btn_menu_inventarios.pack(pady=10)
+    btn_menu_ventas = tk.Button(
+        ventana, text="Menú de Ventas", width=btn_width, height=btn_height,
+        command=lambda: menu_ventas(nivel, ventana, id))
+    btn_menu_ventas.grid(row=0, column=0, pady=10)
+
+    btn_menu_inventarios = tk.Button(
+        ventana, text="Menú de Inventarios", width=btn_width, height=btn_height,
+        command=lambda: menu_inventarios(nivel, ventana, id))
+    btn_menu_inventarios.grid(row=0, column=1, pady=10)
+
+    btn_gestion_clientes = tk.Button(
+        ventana, text="Gestión de Clientes", width=btn_width, height=btn_height,
+        command=lambda: gestion_clientes(nivel, ventana, id))
+    btn_gestion_clientes.grid(row=0, column=2, pady=10)
+
+    if nivel == 3:
+        btn_gestion_usuarios = tk.Button(
+            ventana, text="Gestión de Usuarios", width=btn_width, height=btn_height,
+            command=lambda: gestion_usuarios(nivel, ventana, id))
+        btn_gestion_usuarios.grid(row=0, column=3, pady=10)
 
     if nivel >= 2:
-        btn_generacion_reportes = tk.Button(ventana, text="Generación de Reportes", command=lambda:generacion_reportes(nivel,ventana,id))
-        btn_generacion_reportes.pack(pady=10)
+        btn_generacion_reportes = tk.Button(
+            ventana, text="Generación de Reportes", width=btn_width, height=btn_height,
+            command=lambda: generacion_reportes(nivel, ventana, id))
+        btn_generacion_reportes.grid(row=1, column=1, pady=10)
 
     if nivel == 3:
-        btn_gestion_usuarios = tk.Button(ventana, text="Gestión de Usuarios", command=lambda:gestion_usuarios(nivel, ventana,id))
-        btn_gestion_usuarios.pack(pady=10)
+        btn_registro_actividades = tk.Button(
+            ventana, text="Registro de Actividades", width=btn_width, height=btn_height,
+            command=lambda: registro_actividades(nivel, ventana, id))
+        btn_registro_actividades.grid(row=1, column=2, pady=10)
 
-    btn_gestion_clientes = tk.Button(ventana, text="Gestión de Clientes", command=lambda:gestion_clientes(nivel, ventana,id))
-    btn_gestion_clientes.pack(pady=10)
+    btn_cerrar_sesion = tk.Button(
+        ventana, text="Cerrar Sesión", width=btn_width, height=btn_height,
+        command=lambda: cerrar_sesion(ventana))
+    btn_cerrar_sesion.grid(row=2, column=1, pady=10)
 
-    if nivel == 3:
-        btn_registro_actividades = tk.Button(ventana, text="Registro de Actividades",
-                                             command=lambda : registro_actividades(nivel, ventana, id))
-        btn_registro_actividades.pack(pady=10)
-
-    btn_cerrar_sesion = tk.Button(ventana, text="Cerrar Sesión", command=lambda: cerrar_sesion(ventana))
-    btn_cerrar_sesion.pack(pady=10)
-
-    btn_salir = tk.Button(ventana, text="Salir", command=lambda: salir(ventana))
-    btn_salir.pack(pady=10)
+    btn_salir = tk.Button(
+        ventana, text="Salir", width=btn_width, height=btn_height,
+        command=lambda: salir(ventana))
+    btn_salir.grid(row=2, column=2, pady=10)
 
     ventana.mainloop()
 
+
+main(2, 2)
